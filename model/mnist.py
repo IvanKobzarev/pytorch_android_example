ead6f554a6ef0c85cf0aac41975ef3a3aa1eed14
Ivan Kobzarev <ivankobzarev@fb.com>
2021-07-07 14:29:34 -0700

    nnapi model generation

--- model/mnist.py
+++ model/mnist.py
@@ -5,12 +5,15 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.backends._nnapi.prepare as prepare
 from torchvision import datasets, transforms
 from torch.optim.lr_scheduler import StepLR
 from pathlib import Path
 from torch.utils.mobile_optimizer import optimize_for_mobile
 import yaml
 
+print(torch.version.__version__)
+print(torch.__file__)
 
 class Net(nn.Module):
     def __init__(self):
@@ -27,9 +30,9 @@ class Net(nn.Module):
         x = F.relu(x)
         x = self.conv2(x)
         x = F.relu(x)
-        x = F.max_pool2d(x, 2)
+        x = F.max_pool2d(x, kernel_size=2, stride=2)
         x = self.dropout1(x)
-        x = torch.flatten(x, 1)
+        x = torch.reshape(x, (1, -1))
         x = self.fc1(x)
         x = F.relu(x)
         x = self.dropout2(x)
@@ -106,25 +109,9 @@ def save_model(
         yaml.dump(ops, output)
 
 
-def make_nnapi_model(model, quantize_mode):
-    quantize_core, quantize_iface = {
-        "none": (False, False),
-        "core": (True, False),
-        "full": (True, True),
-    }[quantize_mode]
-
+def make_nnapi_model(model):
     input_float = torch.zeros(1, 1, 28, 28)
     input_tensor = input_float
-
-    # If we're doing a quantized model, we need to trace only the quantized core.
-    # So capture the quantizer and dequantizer, use them to prepare the input,
-    # and replace them with identity modules so we can trace without them.
-    if quantize_core:
-        quantizer = model.quant
-        dequantizer = model.dequant
-        model.quant = torch.nn.Identity()
-        model.dequant = torch.nn.Identity()
-        input_tensor = quantizer(input_float)
 
     # Many NNAPI backends prefer NHWC tensors, so convert our input to channels_last,
     # and set the "nnapi_nhwc" attribute for the converter.
@@ -137,34 +124,7 @@ def make_nnapi_model(model, quantize_mode):
         traced = torch.jit.trace(model, input_tensor)
     nnapi_model = torch.backends._nnapi.prepare.convert_model_to_nnapi(traced, input_tensor)
 
-    # If we're not using a quantized interface, wrap a quant/dequant around the core.
-    if quantize_core and not quantize_iface:
-        nnapi_model = torch.nn.Sequential(quantizer, nnapi_model, dequantizer)
-        model.quant = quantizer
-        model.dequant = dequantizer
-        # Switch back to float input for benchmarking.
-        input_tensor = input_float.contiguous(memory_format=torch.channels_last)
-
-    # Optimize the CPU model to make CPU-vs-NNAPI benchmarks fair.
-    model = torch.utils.mobile_optimizer.optimize_for_mobile(torch.jit.script(model))
-
-    # Bundle sample inputs with the models for easier benchmarking.
-    # This step is optional.
-    class BundleWrapper(torch.nn.Module):
-        def __init__(self, mod):
-            super().__init__()
-            self.mod = mod
-        def forward(self, arg):
-            return self.mod(arg)
-    nnapi_model = torch.jit.script(BundleWrapper(nnapi_model))
-    torch.utils.bundled_inputs.augment_model_with_bundled_inputs(
-        model, [(torch.utils.bundled_inputs.bundle_large_tensor(input_tensor),)])
-    torch.utils.bundled_inputs.augment_model_with_bundled_inputs(
-        nnapi_model, [(torch.utils.bundled_inputs.bundle_large_tensor(input_tensor),)])
-
-    # Save both models.
-    model.save(output_dir_path / ("mobilenetv2-quant_{}-cpu.pt".format(quantize_mode)))
-    nnapi_model.save(output_dir_path / ("mobilenetv2-quant_{}-nnapi.pt".format(quantize_mode)))
+    return nnapi_model
 
 
 
@@ -262,6 +222,7 @@ def main():
             test(model, device, test_loader)
             scheduler.step()
 
+    model.eval()
     save_model(
         model,
         args.save_model_state,
@@ -270,8 +231,13 @@ def main():
         args.save_model_mobile,
         args.save_model_ops)
 
-    for quantize_mode in ["none", "core", "full"]:
-        make_nnapi_model(model, quantize_mode)
+    nnapi_model = make_nnapi_model(model)
+    nnapi_model_script = torch.jit.script(nnapi_model)
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    nnapi_model_path = script_dir + "/output/mnist-nnapi.pt"
+    nnapi_model_script.save(nnapi_model_path)
+    nnapi_model_mobile_path = script_dir + "/output/mnist-nnapi.ptl"
+    nnapi_model._save_for_lite_interpreter(nnapi_model_mobile_path)
 
     model_quantized = torch.quantization.quantize_dynamic(
         model,
