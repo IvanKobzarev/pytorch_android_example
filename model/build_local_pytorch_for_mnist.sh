056f8f4a6740d85d4ef80a06b571175835b0e6e7
Ivan Kobzarev <ivankobzarev@fb.com>
2021-07-08 15:50:21 -0700

    fix

--- model/build_local_pytorch_for_mnist.sh
+++ model/build_local_pytorch_for_mnist.sh
@@ -12,11 +12,9 @@ cd $PYTORCH_ROOT
 
 #FIXME: build for all ABIs, remove x86
 SELECTED_OP_LIST=$ROOT/model/output/mnist-ops-all.yaml \
-  sh ./scripts/build_pytorch_android.sh x86 arm64-v8a
+  sh ./scripts/build_pytorch_android.sh arm64-v8a
 
-unlink -f $ROOT/android/application/app/aars/pytorch_android.aar
-
-ln -s \
+ln -sf \
   $ROOT/third_party/pytorch/android/pytorch_android/build/outputs/aar/pytorch_android-release.aar \
   $ROOT/android/application/app/aars/pytorch_android.aar
 
